b22af94e7fbb3ae30b2ed1280cb0bd5429bb3eb5
FREKAB-Niklas <niklas@frekab.se>
2024-08-09 07:55:34 +0200

    wait

--- run_article.py
+++ run_article.py
@@ -67,9 +67,7 @@ for _ in range(5):
 
 try:
     success_sound.play()
-    success_sound.play()
-    success_sound.play()
-    success_sound.play()
+    pygame.time.wair(3000)
 except Exception as e:
     print(f"Error playing success sound: {e}")
 
