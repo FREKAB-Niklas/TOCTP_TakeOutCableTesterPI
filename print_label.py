782a5e6421e5f1f31142627c5567be6ea51c373e
FREKAB-Niklas <niklas@frekab.se>
2024-09-04 15:45:21 +0200

    test

--- print_label.py
+++ print_label.py
@@ -21,12 +21,12 @@ description_line_1, description_line_2 = split_description(description, 30)
 zpl_template = """
 ^XA
 ^FO00,30^A0N,80,80^FD{PART_NUMBER}^FS  ; Part Number, made larger
-^FO80,130^A0N,40,40^FD{DESCRIPTION_LINE_1}^FS  ; Description Line 1, adjusted position
-^FO80,180^A0N,40,40^FD{DESCRIPTION_LINE_2}^FS  ; Description Line 2, adjusted position
-^FO80,250^A0N,50,50^FDWeek^FS              ; "Week" label, adjusted position
+^FO00,130^A0N,40,40^FD{DESCRIPTION_LINE_1}^FS  ; Description Line 1, adjusted position
+^FO00,180^A0N,40,40^FD{DESCRIPTION_LINE_2}^FS  ; Description Line 2, adjusted position
+^FO00,250^A0N,50,50^FDWeek^FS              ; "Week" label, adjusted position
 ^FO280,250^A0N,50,50^FDREV^FS              ; "REV" label, adjusted position
 ^FO480,250^A0N,50,50^FDSerial^FS           ; "Serial" label, adjusted position
-^FO80,320^A0N,70,70^FD{WEEK}^FS            ; Week number, adjusted position and size
+^FO00,320^A0N,70,70^FD{WEEK}^FS            ; Week number, adjusted position and size
 ^FO280,320^A0N,70,70^FD{REV}^FS            ; Revision, adjusted position and size
 ^FO480,320^A0N,70,70^FD{SERIAL}^FS         ; Serial number, adjusted position and size
 ^XZ
