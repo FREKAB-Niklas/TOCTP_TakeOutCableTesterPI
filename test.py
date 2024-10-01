64b1bfffb3c1a588e2e0fb200603f82cc81afdfd
FREKAB-Niklas <niklas@frekab.se>
2024-10-01 16:44:24 +0200

    test

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 import RPi.GPIO as GPIO
 import time
 
-ENCODER_PIN_A = 17  # You can try a different pin like 22
+ENCODER_PIN_A = 17
 
 def callback(channel):
     print("Edge detected on pin", channel)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(ENCODER_PIN_A, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Print the current state of the pin
+print(f"Pin {ENCODER_PIN_A} state: {GPIO.input(ENCODER_PIN_A)}")
 
 # Adding edge detection with debounce
 GPIO.add_event_detect(ENCODER_PIN_A, GPIO.RISING, callback=callback, bouncetime=200)
