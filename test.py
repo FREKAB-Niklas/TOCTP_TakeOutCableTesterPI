0f7e2e90cb41261a3f28e2d25b62e429d75f63c0
FREKAB-Niklas <niklas@frekab.se>
2024-10-01 16:42:06 +0200

    test

--- test.py
+++ test.py
@@ -1,18 +1,20 @@
 import RPi.GPIO as GPIO
 import time
 
-ENCODER_PIN_A = 17
+ENCODER_PIN_A = 17  # You can try a different pin like 22
+
+def callback(channel):
+    print("Edge detected on pin", channel)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(ENCODER_PIN_A, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+# Adding edge detection with debounce
+GPIO.add_event_detect(ENCODER_PIN_A, GPIO.RISING, callback=callback, bouncetime=200)
+
 try:
     while True:
-        if GPIO.input(ENCODER_PIN_A) == GPIO.LOW:
-            print("Pin is LOW")
-        else:
-            print("Pin is HIGH")
-        time.sleep(0.5)
+        time.sleep(1)
 
 except KeyboardInterrupt:
     print("Stopped by user")
