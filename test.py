1179bd9d3ed97f49ded35c0a6e9759f95ea878ea
FREKAB-Niklas <niklas@frekab.se>
2024-10-01 16:40:24 +0200

    test

--- test.py
+++ test.py
@@ -3,21 +3,19 @@ import time
 
 ENCODER_PIN_A = 17
 
-def callback(channel):
-    print("Edge detected on pin", channel)
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(ENCODER_PIN_A, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-GPIO.add_event_detect(ENCODER_PIN_A, GPIO.BOTH, callback=callback)
-
 try:
     while True:
-        time.sleep(1)
+        if GPIO.input(ENCODER_PIN_A) == GPIO.LOW:
+            print("Pin is LOW")
+        else:
+            print("Pin is HIGH")
+        time.sleep(0.5)
 
 except KeyboardInterrupt:
     print("Stopped by user")
 
 finally:
     GPIO.cleanup()
-
