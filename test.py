7841a256df660fcea220606781c2cee61e81bd0d
FREKAB-Niklas <niklas@frekab.se>
2024-10-01 16:45:44 +0200

    tb

--- test.py
+++ test.py
@@ -3,21 +3,16 @@ import time
 
 ENCODER_PIN_A = 17
 
-def callback(channel):
-    print("Edge detected on pin", channel)
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(ENCODER_PIN_A, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# Print the current state of the pin
-print(f"Pin {ENCODER_PIN_A} state: {GPIO.input(ENCODER_PIN_A)}")
-
-# Adding edge detection with debounce
-GPIO.add_event_detect(ENCODER_PIN_A, GPIO.RISING, callback=callback, bouncetime=200)
-
 try:
     while True:
-        time.sleep(1)
+        if GPIO.input(ENCODER_PIN_A) == GPIO.LOW:
+            print("Pin is LOW")
+        else:
+            print("Pin is HIGH")
+        time.sleep(0.5)
 
 except KeyboardInterrupt:
     print("Stopped by user")
